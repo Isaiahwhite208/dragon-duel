896187561b0d5d2b5f47b781528de171ea3616ab
Isaiah White <isaiahwhite208@gmail.com>
2018-08-15 15:32:21 -0600

    push

--- app/components/duel-service.js
+++ app/components/duel-service.js
@@ -2,15 +2,15 @@ import Player from "../models/Player.js";
 import Dragon from "../models/Dragon.js";
 
 function dragonUrl(url) {
- return 'https://dragon-duel.herokuapp.com/api/dragons' + encodeURIComponent(url)
+  return 'https://dragon-duel.herokuapp.com/api/dragons' + encodeURIComponent(url)
 }
 
 let dragons = {}
 
 export default class DuelService {
- getDragon(draw, drawError) {
-  fetch(dragonUrl("http://dnd5eapi.co/api/spells/"))
-   .then(res => res.json())
-   .then(res => draw(res.results))
- }
+  getDragon(draw, drawError) {
+    fetch(dragonUrl("https://dragon-duel.herokuapp.com/api/dragons"))
+      .then(res => res.json())
+      .then(res => draw(res.results))
+  }
 }
